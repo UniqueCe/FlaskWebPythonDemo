4c08297abaf0ae62efad80cf2cb9430546a238c8
lzhl_UniqueCe <15011529145@163.com>
2019-06-14 15:47:31 +0800

    添加项目

---
+++ untitled/venv/hello.py
@@ -0,0 +1,37 @@
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user')
+def user():
+    return '<h1>hello, user!</h1>'
+
+@app.route('/user/<name>')
+def username(name):
+    return render_template('user.html', name=name)
+
+@app.route('/YX')
+def YX():
+    return '<h1>hello, 闫总! 好骚呀~ </h1>'
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_sever_error(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    print('...')
+    app.run()
