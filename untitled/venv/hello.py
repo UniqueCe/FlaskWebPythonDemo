24f4d98351e92e2490775ff95e00b387d556b18a
lzhl_UniqueCe <15011529145@163.com>
2019-06-19 14:56:17 +0800

    URL重定向

--- untitled/venv/hello.py
+++ untitled/venv/hello.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -10,15 +10,15 @@ from wtforms.validators import DataRequired
 
 
 
-
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config['SECRET_KEY'] = 'Help Me!'
 
+# 表单
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    submit = SubmitField('提交')
 
 
 
@@ -27,21 +27,26 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
+        session['name'] = form.name.data
+        # form.name.data = ''
+        return redirect(url_for('index'))
     return render_template('index.html',
                            current_time=datetime.utcnow(),
                            form=form,
-                           name=name)
+                           name=session.get('name'))
 
 
 @app.route('/user')
 def user():
-    return render_template('user.html', current_time=datetime.utcnow(), name="User")
+    return render_template('user.html',
+                           current_time=datetime.utcnow(),
+                           name="User")
 
 @app.route('/user/<name>')
 def username(name):
-    return render_template('user.html', current_time=datetime.utcnow(), name=name)
+    return render_template('user.html',
+                           current_time=datetime.utcnow(),
+                           name=name)
 
 @app.route('/YX')
 def YX():
@@ -49,11 +54,13 @@ def YX():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html', current_time=datetime.utcnow()), 404
+    return render_template('404.html',
+                           current_time=datetime.utcnow()), 404
 
 @app.errorhandler(500)
 def internal_sever_error(e):
-    return render_template('500.html', current_time=datetime.utcnow()), 500
+    return render_template('500.html',
+                           current_time=datetime.utcnow()), 500
 
 
 
